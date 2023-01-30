3bfd99d490c4702a567d5d7c52c2214a4d8e2f68
s18351 <s18351@pjwstk.edu.pl>
2023-01-30 10:15:54 +0100

    Add project files.

---
+++ LinqTutorials/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var t = LinqTasks.Task4().ToList();
+            LinqTasks.Task14().ToList().ForEach(a => Console.WriteLine(a.Dname));
+
+            //Console.WriteLine(LinqTasks.Task13(new int[]{ 1,1,1,1,1,1,10,1,1,1,1}));
+
+        }
+    }
+}
